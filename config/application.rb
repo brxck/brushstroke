550f9dad662b8123b5981682344f87d2281c1a37
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-09-10 11:24:08 -0700

    Configure Rail to generate javascript

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module Brushstroke
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators.javascript_engine = :js
   end
 end
