47ca5bdd756d3f2b36bbb683d60ad2495032d6ff
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-09-11 10:46:04 -0700

    Implement canvas controlling

--- app/controllers/canvases_controller.rb
+++ app/controllers/canvases_controller.rb
@@ -1,14 +1,23 @@
 class CanvasesController < ApplicationController
   def create
-    @canvas = Canvas.create()
-    redirect_to @canvas
+    @canvas = Canvas.create
+    redirect_to "/c/#{@canvas.code}"
   end
 
   def show
-
+    @canvas = Canvas.find_by(code: params[:code])
   end
 
-  def destroy
+  def find
+  end
 
+  def search
+    @canvas = Canvas.find_by(code: params[:code])
+    if @canvas
+      render :show
+    else
+      flash.now[:danger] = "canvas not found"
+      render :find
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "canvases#create"
+
+  get "/c/:code", to: "canvases#show", as: "canvas"
+  get "/find", to: "canvases#find"
+  post "/search", to: "canvases#search"
 end
