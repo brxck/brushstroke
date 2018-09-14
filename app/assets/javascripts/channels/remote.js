f973e13ddb38348edf2c588c1f0836f55bff7738
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-09-13 17:31:48 -0700

    Split room/remote code

---
+++ app/assets/javascripts/channels/remote.js
@@ -0,0 +1,20 @@
+// Only run on remote pages
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.getElementById("remote") !== null) {
+    subscribeRemote()
+  }
+})
+
+function subscribeRemote () {
+  App.room = App.cable.subscriptions.create("RoomChannel", {
+    connected: function () {
+      alert("success")
+      const gyro = new GyroNorm()
+      gyro.init().then(() => {
+        gyro.start(data => {
+          this.perform("update", { gyro: data })
+        })
+      })
+    }
+  })
+}
--- app/assets/javascripts/channels/room.js
+++ app/assets/javascripts/channels/room.js
@@ -1,47 +1,45 @@
-const gyro = new GyroNorm()
-
-let dataFrame
-let pointer
-
-App.room = App.cable.subscriptions.create("RoomChannel", {
-  connected: function () {
-    // Called when the subscription is ready for use on the server
-    dataFrame = document.getElementById("data-frame")
-    pointer = document.getElementById("pointer")
-
-    gyro.init().then(() => {
-      gyro.start(data => {
-        this.perform("update", { gyro: data })
-      })
-    })
-  },
-
-  disconnected: function () {
-    // Called when the subscription has been terminated by the server
-  },
-
-  received: function (data) {
-    // Called when there's incoming data on the websocket for this channel
-    dataFrame.innerHTML = JSON.stringify(data)
-
-    pointer.style.bottom =
-      angleToPosition(data["data"]["gyro"]["do"]["beta"], 50) + "vh"
-
-    pointer.style.right =
-      angleToPosition(data["data"]["gyro"]["do"]["alpha"], 70) + "vw"
-
-    pointer.style.transform =
-      "rotate(" + data["data"]["gyro"]["do"]["gamma"] + "deg)"
-
-    let acceleration = Math.max(
-      data["data"]["gyro"]["dm"]["beta"],
-      data["data"]["gyro"]["dm"]["alpha"]
-    )
-    acceleration = Math.abs(acceleration)
-    acceleration = (acceleration / 100) * 255
-    pointer.style["background-color"] = "hsl(" + acceleration + ", 100%, 50%)"
+// Only run on room pages
+document.addEventListener("DOMContentLoaded", () => {
+  if (document.getElementById("room") !== null) {
+    subscribeRoom()
   }
 })
+
+function subscribeRoom () {
+  let dataFrame
+  let pointer
+
+  App.room = App.cable.subscriptions.create("RoomChannel", {
+    connected: function () {
+      // Called when the subscription is ready for use on the server
+      console.log("room subscribed")
+      dataFrame = document.getElementById("data-frame")
+      pointer = document.getElementById("pointer")
+    },
+
+    received: function (data) {
+      // Called when there's incoming data on the websocket for this channel
+      dataFrame.innerHTML = JSON.stringify(data)
+
+      pointer.style.bottom =
+        angleToPosition(data["data"]["gyro"]["do"]["beta"], 50) + "vh"
+
+      pointer.style.right =
+        angleToPosition(data["data"]["gyro"]["do"]["alpha"], 70) + "vw"
+
+      pointer.style.transform =
+        "rotate(" + data["data"]["gyro"]["do"]["gamma"] + "deg)"
+
+      let acceleration = Math.max(
+        data["data"]["gyro"]["dm"]["beta"],
+        data["data"]["gyro"]["dm"]["alpha"]
+      )
+      acceleration = Math.abs(acceleration)
+      acceleration = (acceleration / 100) * 255
+      pointer.style["background-color"] = "hsl(" + acceleration + ", 100%, 50%)"
+    }
+  })
+}
 
 function angleToPosition (degree, range) {
   degree += range / 2
