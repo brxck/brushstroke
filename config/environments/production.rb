2400c112f39539fb094873597b4be8d38385f405
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-09-16 16:08:23 -0700

    Configure action cable for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,8 +45,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://brushstroke-brxck.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ 'http://brushstroke.brockmcelroy.com', 'https://brushstroke-brxck.herokuapp.com', 'http://brushstroke-brxck.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
