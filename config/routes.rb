892db0fd3a1a63577112f82b7a9c75b390841c79
Brock McElroy <28877984+brxck@users.noreply.github.com>
2018-09-15 17:55:12 -0700

    Update find page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   get "/c/:code", to: "rooms#show", as: "room"
   get "/c/:code/remote", to: "rooms#remote"
+  get "/find", to: "rooms#find"
   post "/search", to: "rooms#search"
 
   mount ActionCable.server, at: '/cable'
